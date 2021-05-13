b639a489978148b8ddb1acc66cac1225600f9d89
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-05-13 13:25:55 -0300

    Add Card Temperatura

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,23 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Text("Comprimento"),
               ),
             ),
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Card(
+              color: Colors.lightGreen[300],
+              shape: RoundedRectangleBorder(
+                side: BorderSide(
+                  color: Colors.white70,
+                  width: 2,
+                ),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Center(
+                child: Text("Temperatura"),
+              ),
+            ),
+          ),
         ],
       ),
     );
